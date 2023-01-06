c909c41b388d8261bc35c0f48eab089a5e2ba722
BurritoBandit28 <yansalmon@gmail.com>
2023-01-06 23:18:33 +0000

    removed a couple unused imports

--- java/src/main/java/io/github/burritobandit28/pirate_game/Main.java
+++ java/src/main/java/io/github/burritobandit28/pirate_game/Main.java
@@ -5,9 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
-import java.util.ServiceLoader;
 
 public class Main {
 
